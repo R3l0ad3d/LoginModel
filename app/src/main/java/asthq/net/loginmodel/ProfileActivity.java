37ec36780e3a99bad80b7544497ea8a1e02aa250
reloaded <azizur.rahman.bu@gmail.com>
2018-07-16 13:27:24 +0600

    Collapsable toolbar

---
+++ app/src/main/java/asthq/net/loginmodel/ProfileActivity.java
@@ -0,0 +1,26 @@
+package asthq.net.loginmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.AppBarLayout;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private AppBarLayout appBarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        toolbar = findViewById(R.id.toolBarProfile);
+
+        setSupportActionBar(toolbar);
+
+        appBarLayout = findViewById(R.id.appBarLayout);
+    }
+}
